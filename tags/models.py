5b5a9747eaab2fa7115d283e8ca7e15920fa09eb
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2024-02-14 20:18:07 +0000

    impl tag and taggedItem models

--- tags/models.py
+++ tags/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
-# Create your models here.
+
+class Tag(models.Model):
+    title = models.CharField(max_length=200)
+
+
+class TaggedItem(models.Model):
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE, related_name="tagged_items")
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE, related_name="tagged_items")
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey()
